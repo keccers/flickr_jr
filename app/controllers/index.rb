77d95e2cc37644dd641a70c4f56f0eaf55ba2793
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-25 17:57:27 -0500

    Update routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,29 @@
 get '/' do
-
+@photos = Photo.all
   erb :index
 end
+
+get '/login' do
+
+end
+
+post '/login' do
+
+end
+
+
+#######  Photos  #######
+get '/upload' do
+  # Upload Photos
+end
+
+post '/photo_upload' do
+  photo = Photo.new(photo: params[:photo])
+
+  if photo.save
+    redirect to '/'
+  else
+    erb :index
+  end
+end
+
