6bfb411591a77e5d1993622056142bc7dc7cb548
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-25 15:21:18 -0500

    Add associations and vallidations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  has_many :photos
 end
--- db/migrate/20130725144231_create_albums.rb
+++ db/migrate/20130725144231_create_albums.rb
@@ -1,4 +1,10 @@
 class CreateAlbums < ActiveRecord::Migration
   def change
+    create_table :albums do |t|
+      t.string :title
+      t.belongs_to :user
+
+      t.timestamps
+    end
   end
 end
