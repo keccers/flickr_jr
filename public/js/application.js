6675f8c3ee35c210b04dbf4fc934311397c338a0
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-25 22:09:02 -0500

    Add basic AJAX to sign in form

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+ $('#login').on('click', function(event){
+  event.preventDefault;
+  $.post('/login', function(response){
+      console.log(response)
+  });
+ }); 
 });
