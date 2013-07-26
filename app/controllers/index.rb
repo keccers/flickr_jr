937074db83313663b1050e2e935bb9a1ec67d7ff
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-25 22:09:44 -0500

    Build create user and login routes and views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,16 +1,8 @@
 get '/' do
+@user = User.find(session[:id])
 @photos = Photo.all
 erb :index
 end
-
-get '/login' do
-
-end
-
-post '/login' do
-
-end
-
 
 #######  Photos  #######
 get '/upload' do
@@ -26,4 +18,39 @@ post '/photo_upload' do
     erb :index
   end
 end
+
+#######  Users  #######
+
+post '/login' do
+@user = User.authenticate(params[:name], params[:password])
+  if @user
+      if request.xhr?
+      session[:id] = @user.id 
+      erb :_login_form, layout: false
+    else
+      redirect '/'
+    end
+  else
+    @errors = ['This username and password combination is invalid.']
+    @photos = Photo.all
+    erb :index
+  end
+end
+
+
+get '/register' do
+  erb :sign_up
+end
+
+post '/register' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors
+    erb :sign_up
+  end
+end
+
 
