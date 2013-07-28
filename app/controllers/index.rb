d6c6ee6c64fa85863730daabdc0213f38af90d64
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-28 15:05:32 -0500

    Update signup and login forms

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,28 @@
 get '/' do
-@photos = Photo.all
-erb :index
+  @photos = Photo.all
+  erb :index
 end
 
-
 post '/login' do
+  @user = User.authenticate(params[:username],params[:password])
+  session[:id] = @user.id if @user
+  redirect '/'
+end
 
+post '/register' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect '/'
+  else 
+    @errors = @user.errors.full_messages
+    erb :index
+  end
+end
+
+get '/logout' do
+  session[:id] = nil
+  redirect '/'
 end
 
 
--- app/helpers/current_user.rb
+++ app/helpers/current_user.rb
@@ -1,3 +1,3 @@
 def current_user
-  @current_user ||= User.find(session[:id])
+  @current_user ||= User.find_by_id(session[:id])
 end
